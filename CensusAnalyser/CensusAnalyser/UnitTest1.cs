ffe58c1167ded4bb3deb7ba315e34548ff094082
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-08 21:57:50 +0530

    Load the Indian States code information from CSVfile

--- CensusAnalyser/CensusAnalyser/UnitTest1.cs
+++ CensusAnalyser/CensusAnalyser/UnitTest1.cs
@@ -45,6 +45,49 @@ namespace CensusAnalyser
             var ex = Assert.Throws<CensusAnalyserException>(() => CSVStateCensus.CheckDelimiter(Path, "Wrong_Header"));
             Assert.That(ex.mgs, Is.EqualTo(CensusException.Wrong_Delimiter.ToString()));
         }
+
+        //USECASE 2: state code csv
+        [Test]
+        public void GivenNoOfRecords_WhenNumOfRecoder_ShouldReturnsNoOfRecordesInCSV1()
+        {
+            string Path = @"C:\Users\Srinidhi\source\repos\CensersAnaliserProblem\CensersAnaliserProblem\sateCode.csv";
+            int actual = CSVStateCensus.StateLoadData(Path);
+            Assert.AreEqual(30, actual);
+        }
+
+        [Test]
+        public void GivenFilePathIncorrect_WhenCheckFilePath_ShouldReturnsFileNotFoundException1()
+        {
+            var ex = Assert.Throws<FileNotFoundException>(() => CSVStateCode.StateLoadData("Wrong_File_Path"));
+            Assert.That(ex.Message, Is.EqualTo(CensusException.Wrong_File_Path.ToString()));
+        }
+
+
+        [Test]
+        public void GivenFileExtansionIncorrect_WhenCheckFilePath_ShouldReturnsFileNotFoundException1()
+        {
+            string Path = @"C:\Users\Srinidhi\source\repos\CensersAnaliserProblem\CensersAnaliserProblem\sateCode.txt";
+            var ex = Assert.Throws<FileNotFoundException>(() => CSVStateCode.StateLoadData(Path));
+            Assert.That(ex.Message, Is.EqualTo(CensusException.Wrong_File_Path.ToString()));
+        }
+
+
+        [Test]
+        public void GivenWrong_Delimiter_WhenDelimiterIsWrong_ShouldPassWrongDelimiter1()
+        {
+            string Path = @"C:\Users\Srinidhi\source\repos\CensersAnaliserProblem\CensersAnaliserProblem\sateCode.csv";
+            var ex = Assert.Throws<CensusAnalyserException>(() => CSVStateCode.CheckDelimiter(Path, "State,Population,AreaInSqKm,DensityPerSqKm"));
+            Assert.That(ex.mgs, Is.EqualTo(CensusException.Wrong_Delimiter.ToString()));
+        }
+
+
+        [Test]
+        public void GivenWrongHeader_WhenHeaderDontMatch_ShouldPassWrongHeader1()
+        {
+            string Path = @"C:\Users\Srinidhi\source\repos\CensersAnaliserProblem\CensersAnaliserProblem\sateCode.csv";
+            var ex = Assert.Throws<CensusAnalyserException>(() => CSVStateCode.CheckDelimiter(Path, "Wrong_Header"));
+            Assert.That(ex.mgs, Is.EqualTo(CensusException.Wrong_Delimiter.ToString()));
+        }
     }
 }
     
