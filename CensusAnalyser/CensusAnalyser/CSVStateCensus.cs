d4e8d386cea50d35186c3c7d6803e011fb90ba9f
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-09 12:53:04 +0530

    Refactor code by using Map

--- CensusAnalyser/CensusAnalyser/CSVStateCensus.cs
+++ CensusAnalyser/CensusAnalyser/CSVStateCensus.cs
@@ -3,59 +3,65 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Linq;
-using LumenWorks.Framework.IO.Csv;
+
 
 namespace CensusAnalyser
 {
-    class CSVStateCensus
+
+    public class CSVStateCensus : CSVFileBuilder
     {
- 
-        public static int StateLoadData(string filePath)
+
+        public int StateLoadData(string filePath, string header, string delimeter = ",")
         {
-            int numberOfRecord = 0;
-            try
-            {
-                CsvReader csvRecords = new CsvReader(new StreamReader(filePath), true);
-                int fieldCount = csvRecords.FieldCount;
-                string[] headers = csvRecords.GetFieldHeaders();
-                char delimete = csvRecords.Delimiter;
-                // string ArrayList
-                List<string[]> record = new List<string[]>();
-                foreach (var line in File.ReadLines(filePath))
-                {
-                    string[] tempRecord = new string[fieldCount];
-                    csvRecords.CopyCurrentRecordTo(tempRecord);
-                    record.Add(tempRecord);
-                    numberOfRecord++;
-
-
-                }
-                return numberOfRecord;
-            }
-            catch (FileNotFoundException)
-            {
-                throw new FileNotFoundException(CensusException.Wrong_File_Path + "");
-            }
-
-        }
-        public static void CheckDelimiter(string filePath, string header)
-        {
-            string line1 = File.ReadAllText(filePath);
             var file_total = File.ReadLines(filePath);
             string[] line_element = file_total.ToArray();
             if (!line_element[0].Contains(header))
             {
-                throw new CensusAnalyserException(CensusException.Wrong_Delimiter + "");
-            }
-            else
-            {
-                Console.WriteLine("right ");
+                throw new CensusAnalyserException(CensusException.Wrong_Header + "");
             }
 
-            if (!line1.Contains(";"))
+            else if (!file_total.Contains(";"))
             {
                 throw new CensusAnalyserException(CensusException.Wrong_Delimiter + "");
             }
+
+
+            int k = 1;
+            Dictionary<int, Dictionary<string, string>> map = new Dictionary<int, Dictionary<string, string>>();
+            string[] key = line_element[0].Split(",");
+            if (key.Length < 2)
+            {
+                throw new CensusAnalyserException(CensusException.Wrong_Delimiter + "");
+            }
+
+            for (int i = 1; i < line_element.Length; i++)
+            {
+                string[] value = line_element[i].Split(",");
+                Dictionary<string, string> subMap = new Dictionary<string, string>()
+               {
+                   { key[0], value[0] },
+                   { key[1], value[1] },
+                   { key[2], value[2] },
+                   { key[3], value[3] },
+               };
+
+                map.Add(k, subMap);
+                k++;
+            }
+
+            foreach (var data in map)
+            {
+                Console.WriteLine("{");
+                foreach (var data1 in data.Value)
+                {
+                    Console.WriteLine(data1);
+                }
+
+                Console.WriteLine("},");
+            }
+
+            return map.Count;
         }
+
     }
 }
