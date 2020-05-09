50bf116c7e457b93faaa5a8d9be05bc0d3e8a110
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-09 11:42:04 +0530

    List to iterate

---
+++ CensusAnalyser/CensusAnalyser/CSVException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensersAnaliserProblem
+{
+    public enum ExceptionType
+    {
+        CSVFILE_IS_EMPTY,
+        file_is_empty
+    }
+
+    public class CSVException : Exception
+    {
+
+        public string mgs;
+        public CSVException(string message)
+        {
+            this.mgs = message;
+        }
+    }
+
+}
