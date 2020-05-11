329c968b303121f5d1576baff15476fda6fcb8b4
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-11 11:23:13 +0530

    Added : state census data in json format from most Density state state to the last one

---
+++ CensusAnalyser/CensusAnalyser/CSVException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensersAnaliserProblem
+{
+    public enum ExceptionType
+    {
+        CSVFILE_IS_EMPTY
+    }
+
+    public class CSVException : Exception
+    {
+
+        public string mgs;
+        public CSVException(string message)
+        {
+            this.mgs = message;
+        }
+    }
+
+}
